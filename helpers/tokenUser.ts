b39719450573b9d60fe761ed90de07ce5e5ec98a
darshan <darshpansheriya2510@gmail.com>
2024-01-23 16:07:18 +0530

    project-node

--- helpers/tokenUser.ts
+++ helpers/tokenUser.ts
@@ -15,13 +15,13 @@ export const verifyToken = async (request: Request,response: Response,next: Next
         let secretKey: any = process.env.SECRETE_KEY 
         let token: any = request.headers['authorization']?.split(" ")[1];
             let {userId} : any = jwt.verify(token, secretKey);
-            let user = await userModel.findById(userId);
+            let user = await userModel.findOne({_id:userId, isAdmin: false});
             request.user = user;
             if(request.user){
                 next();
             }
             else{
-                response.json({message: "Invalide admin"})
+                response.json({message: "Invalide user"})
             }
     } catch (error) {
         return response.status(500).json({message: "Invalide token"})
